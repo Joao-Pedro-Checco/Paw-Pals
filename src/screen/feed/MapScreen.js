001bcf97b05b9aca2e4e042a92b7acfb066fce15
Joao Pedro <joaochecco@gmail.com>
2024-05-07 20:05:02 -0300

    ajustes na tela do mapa

--- src/screen/feed/MapScreen.js
+++ src/screen/feed/MapScreen.js
@@ -1,23 +1,17 @@
-import {Platform, Text} from 'react-native';
+import {Platform, SafeAreaView, Text, NativeModules, StatusBar, ImageBackground} from 'react-native';
 import {Container} from "../../styles/feed/MapStyles";
 import MapView from 'react-native-maps';
-import { Dimensions } from 'react-native';
-import { PermissionsAndroid } from 'react-native';
-import { StyleSheet, Button } from 'react-native';
-import {FlatList} from "react-native";
-import { useEffect, useState } from 'react';
+import {Dimensions} from 'react-native';
+import {StyleSheet, Button} from 'react-native';
+import {useEffect, useState} from 'react';
 import * as Location from 'expo-location';
-
-
-
-
 
 
 const MapScreen = ({navigation}) => {
     const {width, height} = Dimensions.get('screen');
+    const {StatusBarManager} = NativeModules;
 
-
-   const userLocation = async() => {
+    const userLocation = async () => {
 
         let Location = await Location.getCurrentPositionAsync({enableHighAccuracy: true});
         setMapRegion({
@@ -31,46 +25,41 @@ const MapScreen = ({navigation}) => {
 
     const [location, setLocation] = useState(null);
     const [errorMsg, setErrorMsg] = useState(null);
-  
+
     useEffect(() => {
-      (async () => {
-        
-        let { status } = await Location.requestForegroundPermissionsAsync();
-        if (status !== 'granted') {
-          setErrorMsg('Permission to access location was denied');
-          return;
-        }
-  
-        let location = await Location.getCurrentPositionAsync({});
-        setLocation(location);
-      })();
+        (async () => {
+
+            let {status} = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+        })();
     }, []);
-  
+
     let text = 'Waiting..';
     if (errorMsg) {
-      text = errorMsg;
+        text = errorMsg;
     } else if (location) {
-      text = JSON.stringify(location);
+        text = JSON.stringify(location);
     }
 
 
-
-
-    
-    
-    const onRegionChange = (region) =>{
+    const onRegionChange = (region) => {
         console.log(region);
     };
     return (
-        <Container>
-            <Text>Map Screen teste</Text>
-            <MapView style ={styles.map}
-            onRegionChange={onRegionChange}
-            showsUserLocation={true}
-            followsUserLocation={true}
+        <SafeAreaView
+            style={[styles.container, {paddingTop: Platform.OS === 'android' ? StatusBarManager.HEIGHT : 0}]}>
+            <MapView style={styles.map}
+                     onRegionChange={onRegionChange}
+                     showsUserLocation={true}
+                     followsUserLocation={true}
             ></MapView>
-            <Button title ='Get Location' onPress={userLocation}/>
-        </Container>
+        </SafeAreaView>
     );
 };
 
@@ -78,24 +67,21 @@ export default MapScreen;
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    map: {
-      width:'100%',
-      height: '100%'
-    },
-    mapOverlay: {
-      position: "absolute",
-      bottom: 50,
-      backgroundColor: "#ffffff",
-      borderWidth: 2,
-      borderRadius: 5,
-      padding: 16,
-      left: "25%",
-      width: "50%",
-      textAlign: "center"
+        flex: 1,
+        backgroundColor: '#77BBC4',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }, map: {
+        width: '100%', height: '110%'
+    }, mapOverlay: {
+        position: "absolute",
+        bottom: 50,
+        backgroundColor: "#ffffff",
+        borderWidth: 2,
+        borderRadius: 5,
+        padding: 16,
+        left: "25%",
+        width: "50%",
+        textAlign: "center"
     }
-  });
+});
