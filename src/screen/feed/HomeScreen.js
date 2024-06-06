682023497503d03adaee9797719d723f821e2680
Joao Pedro <joaochecco@gmail.com>
2024-06-06 15:44:57 -0300

    Deus é mais git add *!

--- src/screen/feed/HomeScreen.js
+++ src/screen/feed/HomeScreen.js
@@ -1,66 +1,105 @@
-import {Container} from "../../styles/feed/HomeStyles";
+import { Container } from "../../styles/feed/HomeStyles";
 import PostCard from "./PostCard";
-import {FlatList} from "react-native";
+import { ActivityIndicator, FlatList, View } from "react-native";
+import { useEffect, useState } from "react";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { FIREBASE_DB } from "../../firebase/FirebaseConfig";
+import { Text } from "react-native";
 
-const HomeScreen = ({navigation}) => {
-    const Posts = [
-        {
-            id: 1,
-            userName: "João Pedro",
-            profilePic: require("../../assets/logo.png"),
-            postTime: "4 horas atrás",
-            description: "Esse é o texto da postagem",
-            postImage: require("../../assets/mario.jpeg"),
-            liked: true
-        },
-        {
-            id: 2,
-            userName: "Gustavo de Castro",
-            profilePic: require("../../assets/logo.png"),
-            postTime: "4 horas atrás",
-            description: "Esse é o texto da postagem",
-            postImage: null,
-            liked: true
-        },
-        {
-            id: 3,
-            userName: "Renan Almassan",
-            profilePic: require("../../assets/logo.png"),
-            postTime: "4 horas atrás",
-            description: "Esse é o texto da postagem",
-            postImage: require("../../assets/mario.jpeg"),
-            liked: false
-        },
-        {
-            id: 4,
-            userName: "Lucas Nakamura",
-            profilePic: require("../../assets/logo.png"),
-            postTime: "4 horas atrás",
-            description: "Esse é o texto da postagem",
-            postImage: null,
-            liked: false
-        },
-        {
-            id: 5,
-            userName: "Gabriel Rosa",
-            profilePic: require("../../assets/logo.png"),
-            postTime: "4 hora atrás",
-            description: "Esse é o texto da postagem",
-            postImage: null,
-            liked: false
-        }
-    ];
+const HomeScreen = ({ navigation }) => {
+  const [posts, setPosts] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [formatedPosts, setFormatedPosts] = useState([]);
 
-    return (
-        <Container>
-            <FlatList
-                data={Posts}
-                renderItem={({item}) => <PostCard item={item} />}
-                keyExtractor={item => item.id}
-                showsVerticalScrollIndicator={false}
-            />
-        </Container>
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  useEffect(() => {
+    if (posts.length > 0) {
+      fetchUsersAndFormatPosts();
+    }
+  }, [posts]);
+
+  const fetchPosts = async () => {
+    try {
+      console.log("Fetching posts from Firebase...");
+      const postsResponse = await getPosts();
+      setPosts(postsResponse);
+      console.log("Posts fetched:", postsResponse);
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+    }
+  };
+
+  const fetchUsersAndFormatPosts = async () => {
+    try {
+      const userPromises = posts.map((post) => getUser(post.userId));
+      const usersResponse = await Promise.all(userPromises);
+      const newUsers = usersResponse.flat();
+      setUsers(newUsers);
+
+      console.log("Users fetched:", newUsers);
+
+      const newFormatedPosts = posts.map((post) => {
+        const user = newUsers.find((user) => user.uid === post.userId);
+        return {
+          id: post.id,
+          userName: user ? user.name : "Unknown",
+          profilePic: user ? user.photoURI : null,
+          postTime: post.postTime,
+          description: post.postText,
+          postImage: post.postImg,
+          liked: post.likes,
+        };
+      });
+
+      setFormatedPosts(newFormatedPosts);
+      console.log("Formatted posts:", newFormatedPosts);
+    } catch (error) {
+      console.error("Error fetching users or formatting posts:", error);
+    }
+  };
+
+  const getPosts = async () => {
+    const arr = [];
+    const q = query(collection(FIREBASE_DB, "posts"));
+    const querySnapshot = await getDocs(q);
+
+    querySnapshot.forEach((doc) => {
+      const post = doc.data();
+      arr.push(post);
+    });
+
+    return arr;
+  };
+
+  const getUser = async (userId) => {
+    const userArr = [];
+    const q = query(
+      collection(FIREBASE_DB, "users"),
+      where("uid", "==", userId)
     );
+    const querySnapshot = await getDocs(q);
+
+    querySnapshot.forEach((doc) => {
+      const user = doc.data();
+      userArr.push(user);
+    });
+
+    return userArr;
+  };
+
+  return (
+    <Container>
+      <FlatList
+        data={formatedPosts}
+        renderItem={({ item }) => <PostCard item={item} />}
+        keyExtractor={(item) => item.id}
+        showsVerticalScrollIndicator={false}
+      />
+    </Container>
+  );
 };
 
 export default HomeScreen;
--- src/screen/feed/PostCard.js
+++ src/screen/feed/PostCard.js
@@ -1,58 +1,56 @@
 import {
-    Card,
-    Divider,
-    Interaction,
-    InteractionText,
-    InteractionWrapper,
-    PostImage,
-    PostText,
-    PostTime,
-    ProfilePic,
-    UserName,
-    UserNameWrapper,
-    UserWrapper
+  Card,
+  Divider,
+  Interaction,
+  InteractionText,
+  InteractionWrapper,
+  PostImage,
+  PostText,
+  PostTime,
+  ProfilePic,
+  UserName,
+  UserNameWrapper,
+  UserWrapper,
 } from "../../styles/feed/PostCardStyles";
-import Icon from 'react-native-vector-icons/Ionicons';
-import {useState} from "react";
+import Icon from "react-native-vector-icons/Ionicons";
+import { useState } from "react";
 
-const PostCard = ({item}) => {
-    const [like, setLike] = useState(false);
-    let likeIcon = like ? "heart" : "heart-outline";
-    let likeText = like ? "Curtiu" : "Curtir";
+const PostCard = ({ item }) => {
+  const [like, setLike] = useState(false);
+  let likeIcon = like ? "heart" : "heart-outline";
+  let likeText = like ? "Curtiu" : "Curtir";
 
-    const likePost = () => {
-        setLike(!item.liked);
-    }
+  const likePost = () => {
+    setLike(!item.liked);
+  };
 
-    return (
-        <Card>
-            <UserWrapper>
-                <ProfilePic source={item.profilePic}/>
-                <UserNameWrapper>
-                    <UserName>{item.userName}</UserName>
-                    <PostTime>{item.postTime}</PostTime>
-                </UserNameWrapper>
-            </UserWrapper>
-            <PostText>{item.description}</PostText>
-            {
-                item.postImage == null ? (
-                    <Divider></Divider>
-                ) : (
-                    <PostImage source={item.postImage}></PostImage>
-                )
-            }
-            <InteractionWrapper>
-                <Interaction onPress={likePost}>
-                    <Icon name={likeIcon} size={40} color="#485982"/>
-                    <InteractionText>{likeText}</InteractionText>
-                </Interaction>
-                <Interaction>
-                    <Icon name="chatbubble-outline" size={40} color="#485982"/>
-                    <InteractionText>Comentar</InteractionText>
-                </Interaction>
-            </InteractionWrapper>
-        </Card>
-    );
+  return (
+    <Card>
+      <UserWrapper>
+        <ProfilePic source={item.profilePic} />
+        <UserNameWrapper>
+          <UserName>{item.userName}</UserName>
+          <PostTime>{JSON.stringify(item.postTime)}</PostTime>
+        </UserNameWrapper>
+      </UserWrapper>
+      <PostText>{item.description}</PostText>
+      {item.postImage == null ? (
+        <Divider></Divider>
+      ) : (
+        <PostImage source={item.postImage}></PostImage>
+      )}
+      <InteractionWrapper>
+        <Interaction onPress={likePost}>
+          <Icon name={likeIcon} size={40} color="#485982" />
+          <InteractionText>{likeText}</InteractionText>
+        </Interaction>
+        <Interaction>
+          <Icon name="chatbubble-outline" size={40} color="#485982" />
+          <InteractionText>Comentar</InteractionText>
+        </Interaction>
+      </InteractionWrapper>
+    </Card>
+  );
 };
 
 export default PostCard;
